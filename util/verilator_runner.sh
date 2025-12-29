99ab3076f75abec848a9a68c534d7c3deafb71c0
Alice Ziuziakowska <a.ziuziakowska@lowrisc.org>
2025-12-29 10:31:46 +0000

    verilator_runner: update shebang line to work on nixos CI runners
    Signed-off-by: Alice Ziuziakowska <a.ziuziakowska@lowrisc.org>

--- util/verilator_runner.sh
+++ util/verilator_runner.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env -S bash -eux
 # Copyright lowRISC contributors (COSMIC project).
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
@@ -7,16 +7,15 @@ ROOT_DIR=$(dirname "$0")/..
 LOG_FILE="uart0.log"
 
 timeout 5m $ROOT_DIR/build/lowrisc_mocha_top_chip_system_0/sim-verilator/Vtop_chip_verilator \
-  -t -E $1 > /dev/null 2>&1 
+  -t -E $1 > /dev/null 2>&1
 
 if grep -q "TEST RESULT: FAIL" "$LOG_FILE"; then
   echo "Test failed"
   exit 1
 elif grep -q "TEST RESULT: PASSED" "$LOG_FILE"; then
-  echo "Test passed" 
+  echo "Test passed"
   exit 0
 fi
 
 echo "Simulation crashed"
 exit 2
-

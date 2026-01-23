d0388b823e2c004423c74ac4a2ea7c2bce489262
Douglas Reis <doreis@lowrisc.org>
2026-01-23 10:52:32 +0000

    [ci] Speed up verilator run by disabling traces

--- util/verilator_runner.sh
+++ util/verilator_runner.sh
@@ -7,7 +7,7 @@ ROOT_DIR=$(dirname "$0")/..
 LOG_FILE="uart0.log"
 
 timeout 5m $ROOT_DIR/build/lowrisc_mocha_top_chip_system_0/sim-verilator/Vtop_chip_verilator \
-  -t -E $1 > /dev/null 2>&1
+  -E $1 > /dev/null 2>&1
 
 if grep -q "TEST RESULT: FAIL" "$LOG_FILE"; then
   echo "Test failed"
